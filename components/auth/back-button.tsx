5048f4499c380f6e88fec8b156abeca0377ae1c8
Nayan <nayansinghal393@gmail.com>
2024-01-24 22:15:42 +0530

    Login Form UI

---
+++ components/auth/back-button.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+interface BackButtonProps {
+  href: string;
+  label: string;
+}
+
+const BackButton = ({ href, label }: BackButtonProps) => {
+  return (
+    <Button variant="link" size="sm" className="font-normal w-full" asChild>
+      <Link href={href}>{label}</Link>
+    </Button>
+  );
+};
+
+export default BackButton;
---
+++ components/auth/login-button.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => {
+  const router = useRouter();
+  const onClick = () => {
+    router.push("/auth/login");
+  };
+
+  if (mode === "modal") {
+    return <span>MODAL NEEDS TO BE CREATED</span>;
+  }
+
+  return (
+    <span className="cursor-pointer" onClick={onClick}>
+      {children}
+    </span>
+  );
+};
+
+export default LoginButton;
