b361d9a9adfe1a4bfb75e3e02b2cb4863fb64e0e
Nayan <nayansinghal393@gmail.com>
2024-01-26 11:10:19 +0530

    Callbacks

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { prismadb } from "./lib/prismadb";
+import { getUserById } from "./data/user";
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +10,26 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }: any) {
+      // When user will we authenticated, jwt function will execute first and then session function will execute which will add 2 new properties - userId as id and userRole as role and will send it to the client
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+      if (session.user && token.role) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      // I can create my own property in token which will autometicallly pass in the token of session function
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(prismadb),
   session: { strategy: "jwt" },
   ...authConfig,
